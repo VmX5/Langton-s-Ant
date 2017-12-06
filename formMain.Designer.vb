e6c457b3bc7da9de9e54b1fc5513a4ee1f00ecb1
VmX5 <SockMonkeyboy@hotmail.com>
2017-12-05 21:11:39 -0500

    Initial Commit

---
+++ formMain.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class formMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnRestart = New System.Windows.Forms.Button()
+        Me.btnStep = New System.Windows.Forms.Button()
+        Me.numSteps = New System.Windows.Forms.NumericUpDown()
+        Me.btnPlay = New System.Windows.Forms.Button()
+        Me.btnStartTimer = New System.Windows.Forms.Button()
+        Me.btnStopTimer = New System.Windows.Forms.Button()
+        Me.timerStep = New System.Windows.Forms.Timer(Me.components)
+        Me.GameField1 = New Langtons_Ant.gameField()
+        CType(Me.numSteps, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnRestart
+        '
+        Me.btnRestart.Location = New System.Drawing.Point(290, 339)
+        Me.btnRestart.Name = "btnRestart"
+        Me.btnRestart.Size = New System.Drawing.Size(75, 23)
+        Me.btnRestart.TabIndex = 1
+        Me.btnRestart.Text = "restart"
+        Me.btnRestart.UseVisualStyleBackColor = True
+        '
+        'btnStep
+        '
+        Me.btnStep.Location = New System.Drawing.Point(371, 339)
+        Me.btnStep.Name = "btnStep"
+        Me.btnStep.Size = New System.Drawing.Size(75, 23)
+        Me.btnStep.TabIndex = 2
+        Me.btnStep.Text = "step once"
+        Me.btnStep.UseVisualStyleBackColor = True
+        '
+        'numSteps
+        '
+        Me.numSteps.Location = New System.Drawing.Point(452, 342)
+        Me.numSteps.Maximum = New Decimal(New Integer() {100000000, 0, 0, 0})
+        Me.numSteps.Name = "numSteps"
+        Me.numSteps.Size = New System.Drawing.Size(120, 20)
+        Me.numSteps.TabIndex = 3
+        '
+        'btnPlay
+        '
+        Me.btnPlay.Location = New System.Drawing.Point(578, 339)
+        Me.btnPlay.Name = "btnPlay"
+        Me.btnPlay.Size = New System.Drawing.Size(75, 23)
+        Me.btnPlay.TabIndex = 4
+        Me.btnPlay.Text = "play steps"
+        Me.btnPlay.UseVisualStyleBackColor = True
+        '
+        'btnStartTimer
+        '
+        Me.btnStartTimer.Location = New System.Drawing.Point(12, 339)
+        Me.btnStartTimer.Name = "btnStartTimer"
+        Me.btnStartTimer.Size = New System.Drawing.Size(75, 23)
+        Me.btnStartTimer.TabIndex = 5
+        Me.btnStartTimer.Text = "start timer"
+        Me.btnStartTimer.UseVisualStyleBackColor = True
+        '
+        'btnStopTimer
+        '
+        Me.btnStopTimer.Location = New System.Drawing.Point(93, 339)
+        Me.btnStopTimer.Name = "btnStopTimer"
+        Me.btnStopTimer.Size = New System.Drawing.Size(75, 23)
+        Me.btnStopTimer.TabIndex = 6
+        Me.btnStopTimer.Text = "stop timer"
+        Me.btnStopTimer.UseVisualStyleBackColor = True
+        '
+        'timerStep
+        '
+        Me.timerStep.Interval = 200
+        '
+        'GameField1
+        '
+        Me.GameField1.Location = New System.Drawing.Point(12, 12)
+        Me.GameField1.Name = "GameField1"
+        Me.GameField1.Size = New System.Drawing.Size(641, 321)
+        Me.GameField1.TabIndex = 0
+        Me.GameField1.Text = "GameField1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(659, 368)
+        Me.Controls.Add(Me.btnStopTimer)
+        Me.Controls.Add(Me.btnStartTimer)
+        Me.Controls.Add(Me.btnPlay)
+        Me.Controls.Add(Me.numSteps)
+        Me.Controls.Add(Me.btnStep)
+        Me.Controls.Add(Me.btnRestart)
+        Me.Controls.Add(Me.GameField1)
+        Me.Name = "Form1"
+        Me.Text = "Langton's Ant"
+        CType(Me.numSteps, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GameField1 As Langtons_Ant.gameField
+    Friend WithEvents btnRestart As System.Windows.Forms.Button
+    Friend WithEvents btnStep As System.Windows.Forms.Button
+    Friend WithEvents numSteps As System.Windows.Forms.NumericUpDown
+    Friend WithEvents btnPlay As System.Windows.Forms.Button
+    Friend WithEvents btnStartTimer As System.Windows.Forms.Button
+    Friend WithEvents btnStopTimer As System.Windows.Forms.Button
+    Friend WithEvents timerStep As System.Windows.Forms.Timer
+
+End Class
---
+++ formMain.vb
@@ -0,0 +1,138 @@
+﻿Public Class formMain
+
+    Private Sub btnPlay_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
+        GameField1.playLife(numSteps.Value)
+    End Sub
+
+    Private Sub btnStep_Click(sender As Object, e As EventArgs) Handles btnStep.Click
+        GameField1.stepForward()
+    End Sub
+
+    Private Sub btnRestart_Click(sender As Object, e As EventArgs) Handles btnRestart.Click
+        GameField1.restart()
+    End Sub
+
+    Private Sub timerStep_Tick(sender As Object, e As EventArgs) Handles timerStep.Tick
+        GameField1.stepForward()
+    End Sub
+
+    Private Sub btnStartTimer_Click(sender As Object, e As EventArgs) Handles btnStartTimer.Click
+        timerStep.Start()
+    End Sub
+
+    Private Sub btnStopTimer_Click(sender As Object, e As EventArgs) Handles btnStopTimer.Click
+        timerStep.Stop()
+    End Sub
+End Class
+
+Public Class gameField
+    Inherits Control
+
+    Private initialPoint As New Point(16, 13)
+    Private usedTiles As New List(Of Point)
+    Private ant As New Ant(initialPoint, Direction.West)
+
+    Sub New()
+        DoubleBuffered = True
+    End Sub
+
+    Public Sub restart()
+        usedTiles.Clear()
+        ant = New Ant(initialPoint, Direction.West)
+    End Sub
+
+    Protected Overrides Sub OnPaint(e As PaintEventArgs)
+        MyBase.OnPaint(e)
+        Dim g As Graphics = e.Graphics
+        g.Clear(Color.White)
+
+        For Each tile As Point In usedTiles
+            g.FillRectangle(Brushes.Gray, tile.X * 10, tile.Y * 10, 10, 10)
+        Next
+
+        g.FillRectangle(Brushes.Red, ant.location.X * 10, ant.location.Y * 10, 10, 10)
+        Select Case ant.direction
+            Case Direction.North
+                g.FillRectangle(Brushes.Black, ant.location.X * 10 + 2, ant.location.Y * 10 + 2, 7, 2)
+            Case Direction.East
+                g.FillRectangle(Brushes.Black, ant.location.X * 10 + 6, ant.location.Y * 10 + 2, 2, 7)
+            Case Direction.South
+                g.FillRectangle(Brushes.Black, ant.location.X * 10 + 2, ant.location.Y * 10 + 6, 7, 2)
+            Case Direction.West
+                g.FillRectangle(Brushes.Black, ant.location.X * 10 + 2, ant.location.Y * 10 + 2, 2, 7)
+        End Select
+ 
+        For y As Integer = 0 To Height / 10
+            g.DrawLine(Pens.Black, New Point(0, y * 10), New Point(Width, y * 10))
+        Next
+        For x As Integer = 0 To Width / 10
+            g.DrawLine(Pens.Black, New Point(x * 10, 0), New Point(x * 10, Width))
+        Next
+    End Sub
+
+    Public Sub playLife(x As Integer)
+        For i As Integer = 0 To x - 1
+            stepForward()
+        Next
+        Invalidate()
+    End Sub 
+
+    Public Sub stepForward()
+        If (usedTiles.Contains(New Point(ant.location))) Then
+            usedTiles.Remove(ant.location)
+            Select Case ant.direction
+                Case Direction.North
+                    ant.location = New Point(ant.location.X - 1, ant.location.Y)
+                    ant.direction = Direction.West
+                Case Direction.East
+                    ant.location = New Point(ant.location.X, ant.location.Y - 1)
+                    ant.direction = Direction.North
+                Case Direction.South
+                    ant.location = New Point(ant.location.X + 1, ant.location.Y)
+                    ant.direction = Direction.East
+                Case Direction.West
+                    ant.location = New Point(ant.location.X, ant.location.Y + 1)
+                    ant.direction = Direction.South
+            End Select
+        Else
+            usedTiles.Add(New Point(ant.location))
+            Select Case ant.direction
+                Case Direction.North
+                    ant.location = New Point(ant.location.X + 1, ant.location.Y)
+                    ant.direction = Direction.East
+                Case Direction.East
+                    ant.location = New Point(ant.location.X, ant.location.Y + 1)
+                    ant.direction = Direction.South
+                Case Direction.South
+                    ant.location = New Point(ant.location.X - 1, ant.location.Y)
+                    ant.direction = Direction.West
+                Case Direction.West
+                    ant.location = New Point(ant.location.X, ant.location.Y - 1)
+                    ant.direction = Direction.North
+            End Select
+        End If
+        Invalidate()
+    End Sub
+
+    Protected Overrides Sub OnMouseDown(e As MouseEventArgs)
+        MyBase.OnMouseDown(e)
+        ant.location = New Point(Math.Floor(e.X / 10), Math.Floor(e.Y / 10))
+        Invalidate()
+    End Sub
+End Class
+
+Public Class Ant
+    Public Property location As Point
+    Public Property direction As Direction
+    Sub New(loc As Point, dir As Direction)
+        location = loc
+        direction = dir
+    End Sub
+End Class
+
+Public Enum Direction
+    North = 1
+    East = 2
+    South = 3
+    West = 4
+End Enum
